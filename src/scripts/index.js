094c43b957b6f88131c28627acccce6a61085f15
Sandi Junker <sandi.junker@outlook.com>
2024-11-21 08:13:40 -0600

    web app additions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,19 @@
-var esriConfig = {
-    apiKey: import.meta.env.ArcGIS_API_Key
-};
+import { ApiKeyManager } from "@esri/arcgis-rest-request";
+import WebMap from "@arcgis/core/WebMap";
+import MapView from "@arcgis/core/views/MapView";
+import "../styles/index.css";
+
+const accessToken = import.meta.env.ArcGIS_API_Key
+const authentication = ApiKeyManager.fromKey(accessToken);
+
+//Map and Map View
+const webmap = new WebMap({
+    portalItem: {
+        id: "f84dfb63c5254901b58a8101958d9085"
+    }
+});
+
+const view = new MapView({
+    container: "my-map",
+    map: webmap
+});
