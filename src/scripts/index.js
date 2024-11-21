1c6ea0897ed52689c001b7ee1d1173eb6a8e106f
Sandi Junker <sandi.junker@outlook.com>
2024-11-20 22:07:24 -0600

    setting up npm dependencies

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,18 @@
+import "../styles/index.css";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
 
+const map = new Map({
+  basemap: "topo-vector",
+});
+
+const view = new MapView({
+  container: "map-div",
+  map: map,
+  zoom: 4,
+  center: [15, 65],
+});
 
 var esriConfig = {
-    apiKey: "YOUR_ACCESS_TOKEN"
+  apiKey: "YOUR_ACCESS_TOKEN",
 };
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "src/",
+  root: "src/",
 
-    build: {
-        outDir: "../dist",
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "src/index.html")
-            },
-        },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
     },
+  },
 });
