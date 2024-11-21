8101a8a57dfe404cc6d5f5aefe848ea543350aba
Sandi Junker <sandi.junker@outlook.com>
2024-11-20 23:58:40 -0600

    Get map on screen

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,3 @@
-import "../styles/index.css";
-import Map from "@arcgis/core/Map";
-import MapView from "@arcgis/core/views/MapView";
-
-const map = new Map({
-  basemap: "topo-vector",
-});
-
-const view = new MapView({
-  container: "map-div",
-  map: map,
-  zoom: 4,
-  center: [15, 65],
-});
-
 var esriConfig = {
-  apiKey: "YOUR_ACCESS_TOKEN",
+    apiKey: import.meta.env.ArcGIS_API_Key
 };
